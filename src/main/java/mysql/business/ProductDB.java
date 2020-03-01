4716bf2d511b84d3c3b0c0b2a59ee856432c9e63
DevOps <devops324423@gmail.com>
2020-03-01 22:32:50 +0300

    ew

--- src/main/java/mysql/business/ProductDB.java
+++ src/main/java/mysql/business/ProductDB.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 
 public class ProductDB {
 	
-    private static String url = "jdbc:mysql://192.168.0.104:3306/productdb";
-    private static String username = "test";
-    private static String password = "12345";
+    private static String url = "jdbc:mysql://192.168.0.105:3306/productdb";
+    private static String username = "root";
+    private static String password = "12131415";
 	
     public static ArrayList<Product> select() {
         ArrayList<Product> products = new ArrayList<Product>();
